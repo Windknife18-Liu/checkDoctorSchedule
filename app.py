680d2acb905618b54fb78728be33ac9de99254c9
Windknife18-Liu <74760869+Windknife18-Liu@users.noreply.github.com>
2026-01-29 18:53:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Render via GitHub!"
+    return "Hello from Render via GitHub!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
